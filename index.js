09f42a40df3796b7166029b43ef02f284c7556ad
Ajmal Ahamed <ajmalahmed123@gmail.com>
2020-06-16 12:52:30 +0530

    code formatting

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-require('dotenv').config({ path: __dirname + '/.env' })
+require('dotenv').config({ path: __dirname + '/.env' })//load env variables
 const twilio = require("./TwilioNotification")
 const to_number = 'desinationNumber'
 var otp = Math.floor(100000 + Math.random() * 900000)//random number b/w 100000 & 999999 (inclusive)
-var splittedotp = otp.toString().split('').join(' ');
-//we like to here OTP's as one-two-three rather than one hundred and twenty three
-const voicemsg = 'your one time password for secure access is ' + splittedotp
 const smsmsg = 'your one time password for secure access is ' + otp
+//we like to hear voice OTP's as one-two-three rather than one hundred and twenty three
+var splittedotp = otp.toString().split('').join(' ');
+const voicemsg = 'your one time password for secure access is ' + splittedotp
+
 
 //send a voice notification
 twilio.sendVoiceNotification(to_number, voicemsg).then((sid) => {
